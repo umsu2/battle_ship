ceec0163ed669fc51b701c111a6cbd5d6c6ac854
Yang Su <yangsu.personal@gmail.com>
2017-05-23 11:40:34 -0500

    adding tests and views and add routing for home page view

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
-
+from django.urls import resolve
+from lists.views import home_page
 # Create your tests here.
 
-class SmokeTest(TestCase):
-    def test_blah(self):
-        self.assertTrue(False)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEquals(found.func, home_page)

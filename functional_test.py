be1692b5dfc9fe82b13e89a3c1b743cb196396bc
Yang Su <yangsu.personal@gmail.com>
2017-05-23 17:24:33 -0500

    adding fucntional test to check if we can input to-do item

--- functional_test.py
+++ functional_test.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 import os
 import unittest
 #points to the path where the chromedriver is installed
+from selenium.webdriver.common.keys import Keys
+
 chrome_driver_path = "/home/yang/Downloads"
 os.environ["PATH"] += os.pathsep + chrome_driver_path
 
@@ -22,16 +24,28 @@ class NewVisitorTest(unittest.TestCase):
 
         # Bob notices the title says it is a to-do list
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-
-        self.fail('Finish the Test')
         # She is invited to enter a to-do item straight away
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feather')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -45,6 +59,6 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-
+        self.fail('Finish the Test')
 if __name__ == '__main__':
     unittest.main(warnings='ignore')

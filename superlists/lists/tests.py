ae8935709d71653ca15652583bd34f956feb5a90
Yang Su <yangsu.personal@gmail.com>
2017-05-23 17:57:47 -0500

    adding front html for the todo list

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,4 +1,5 @@
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
@@ -9,6 +10,12 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEquals(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode('utf-8'), expected_html)
 
     def test_home_page_return_real_html_response(self):
         request = HttpRequest()

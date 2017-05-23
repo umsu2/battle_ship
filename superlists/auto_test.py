745ce8061b85cb5fb06cc99e22f06f350a98d75c
Yang Su <yangsu.personal@gmail.com>
2017-05-23 15:25:16 -0500

    adding auto test to run django tests all together and added more tests and home page view

---
+++ superlists/auto_test.py
@@ -0,0 +1,13 @@
+
+
+import os
+import unittest
+
+
+class RUN_ALL_TEST(unittest.TestCase):
+
+    def test_run_all(self):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
+        argv  = ['/home/yang/Desktop/battle_ship/superlists/manage.py', 'test']
+        from django.core.management import execute_from_command_line
+        execute_from_command_line(argv)
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
@@ -8,3 +9,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEquals(found.func, home_page)
+
+    def test_home_page_return_real_html_response(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))

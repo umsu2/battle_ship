047dd2a116d8908740e2410af1c6ba1f528ddbf0
Yang Su <yangsu.personal@gmail.com>
2017-05-25 09:11:12 -0500

    updating functional test and auto test runner for  pycharm

--- superlists/auto_test.py
+++ superlists/auto_test.py
@@ -8,6 +8,6 @@ class RUN_ALL_TEST(unittest.TestCase):
 
     def test_run_all(self):
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
-        argv  = ['/home/yang/Desktop/battle_ship/superlists/manage.py', 'test']
+        argv  = ['/home/yang/Desktop/battle_ship/superlists/manage.py', 'test', 'lists']
         from django.core.management import execute_from_command_line
         execute_from_command_line(argv)

c8b90f53aa2f7915cde91ef63cb332619d927d8d
Yang Su <yangsu.personal@gmail.com>
2017-05-25 14:48:08 -0500

    refactoring the url routing

---
+++ superlists/lists/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib import admin
+from lists import views
+
+urlpatterns = [
+    # url(r'^admin/', admin.site.urls),
+    url(r'^(\d+)/$', views.view_list, name= "view_list"),
+    url(r'^new$', views.new_list, name= "new_list"),
+    url(r'^(\d+)/add_item$', views.add_item, name= "add_item")
+]
